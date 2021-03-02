b1dbbe65e859c69a937cdc9b0cc23a0f024b6789
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2021-03-02 17:23:57 -0500

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,22 @@
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/.irbrc ~/.irbrc
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+if ! command -v rg &> /dev/null; then
+  sudo apt-get install -y ripgrep
+fi
+
+if ! command -v fd &> /dev/null; then
+  sudo apt-get install -y fd
+fi
+
+if ! command -v fzf &> /dev/null; then
+  sudo apt-get install -y fd
+fi
+
+zsh && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
