9d3fea441a2a1d1b6755eae534b46033304e8d73
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2021-12-10 17:01:35 -0500

    Fix PlugInstall for nvim

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ if [ $SPIN ]; then
   if command -v nvim &> /dev/null; then
     mkdir -p $HOME/.config/nvim
     ln -sf $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
-    vim +'PlugInstall --sync' +qa
+    nvim --headless +PlugInstall +qall
   fi
 fi
 
