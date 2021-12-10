3aa033664a126a55b955d7418b01ca520fe5b0f4
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2021-12-10 16:16:49 -0500

    install fixes

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+set -ex
+
 for FILE in irbrc gitignore gitconfig vimrc tmux.conf
 do
   ln -sf ~/dotfiles/$FILE ~/.$FILE
@@ -11,16 +13,16 @@ mkdir -p $HOME/.vim/undo
 
 if [ $SPIN ]; then
   if ! command -v rg &> /dev/null; then
-    sudo apt-get install -y ripgrep
+    sudo apt-get install -y -o DPkg::Lock::Timeout=600 ripgrep
   fi
 
   if ! command -v fd &> /dev/null; then
-    sudo apt-get install fd-find
-    ln -s $(which fdfind) /usr/local/bin/fd
+    sudo apt-get install -o DPkg::Lock::Timeout=600 fd-find
+    sudo ln -s $(which fdfind) /usr/local/bin/fd
   fi
 
   if ! command -v fzf &> /dev/null; then
-    sudo apt-get install fzf
+    sudo apt-get install -o DPkg::Lock::Timeout=600 fzf
   fi
 
   echo "StreamLocalBindUnlink yes" | sudo tee -a /etc/ssh/sshd_config
