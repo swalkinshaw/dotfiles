9efd2d121ed39d1856b8a39144a4504268a88527
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2021-12-10 16:56:32 -0500

    More fixes

--- install.sh
+++ install.sh
@@ -2,13 +2,12 @@ set -ex
 
 for FILE in irbrc gitignore gitconfig vimrc tmux.conf
 do
-  ln -sf ~/dotfiles/$FILE ~/.$FILE
+  ln -sf $HOME/dotfiles/$FILE $HOME/.$FILE
 done
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-vim +'PlugInstall --sync' +qa
 mkdir -p $HOME/.vim/undo
 
 if [ $SPIN ]; then
@@ -27,11 +26,6 @@ if [ $SPIN ]; then
     sudo apt-get install -o DPkg::Lock::Timeout=600 fzf
   fi
 
-  echo "StreamLocalBindUnlink yes" | sudo tee -a /etc/ssh/sshd_config
-  sudo service ssh restart
-  rm -rf ~/.gnupg/S*
-
-
   if command -v nvim &> /dev/null; then
     mkdir -p $HOME/.config/nvim
     ln -sf $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
@@ -43,8 +37,8 @@ if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
   zsh && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/zpreztorc ~/.zpreztorc
+ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -sf $HOME/dotfiles/zpreztorc $HOME/.zpreztorc
 
 source $HOME/.zshrc
 
