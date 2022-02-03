544ac8392c6ac5e9e94954460cb127ec3ebfa80c
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2022-02-03 10:30:56 -0500

    Remove tmux conf sourcing

--- install.sh
+++ install.sh
@@ -11,8 +11,6 @@ do
   ln -sf $DOTFILES_PATH/$FILE $HOME/.$FILE
 done
 
-tmux source-file ~/.tmux.conf
-
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 mkdir -p $HOME/.vim/undo
